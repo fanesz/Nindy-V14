db38a571b57c0da701db1b8a86650a282cff43ce
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-17 20:39:01 +0700

    fix Voice Logs #2

--- src/events/VoiceStateUpdate.js
+++ src/events/VoiceStateUpdate.js
@@ -1,4 +1,5 @@
-const { Events } = require("discord.js")
+const { Events } = require("discord.js");
+const config = require("../config");
 
 module.exports = {
   name: Events.VoiceStateUpdate,
@@ -6,19 +7,17 @@ module.exports = {
   async execute(oldState, newState) {
     const client = oldState.client
 
-    console.log(oldState.channelId);
-    console.log(newState.channelId);
-
     const user = oldState.member.id;
     const oldChannel = oldState.channel;
     const newChannel = newState.channel;
 
     // voice log
+    if (newState.guild.id !== config.guildID) return;
     if (oldChannel != null && newChannel == null) { // left 
       client.voicelog(user, ':outbox_tray:', "leaving", `from <#${oldState.channelId}>`)
     } else if (oldChannel == null && newChannel != null) { // join
       client.voicelog(user, ':inbox_tray:', "joining", `to <#${newState.channelId}>`)
-    } else if (oldChannel != null && newChannel != null) { // move
+    } else if (oldChannel != null && newChannel != null && oldChannel != newChannel) { // move
       client.voicelog(user, ':left_right_arrow:', "moving", `from <#${oldState.channelId}> to <#${newState.channelId}>`)
     }
 
