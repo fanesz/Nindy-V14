c1f7dabe7e2f0c68c8f26e374e7a13e7022f5c37
Vanezkun <fanes23.pratama@mhs.mdp.ac.id>
2023-09-20 11:56:27 +0700

    FEAT: NSFW Role Command

--- src/sharedCode/_NSFWRole.js
+++ src/sharedCode/_NSFWRole.js
@@ -1,25 +1,27 @@
+const config = require("../config");
+
 module.exports = {
   name: "nsfw",
   run: async (client, message, args, interaction) => {
 
     const NSFWRoleID = '802892741355896842' // debug 1043736549814194196 | ntc 802892741355896842
-    
-    const setRole = async(user) => {
-      if(user.member.roles.cache.find(r => r.id === NSFWRoleID) === undefined) {
-        await user.member.roles.add(NSFWRoleID);
-        return '<:nindy_yes:977817511821213757> done, role NSFW **added**!'
-      } else {
-        await user.member.roles.remove(NSFWRoleID);
-        return '<:nindy_yes:977817511821213757> done, role NSFW **removed**!'
-      }
+
+    const commandType = interaction ? interaction : message
+    if (commandType.guildId !== config.guildID) return;
+    let replyMessage;
+
+    if (commandType.member.roles.cache.find(r => r.id === NSFWRoleID) === undefined) {
+      await commandType.member.roles.add(NSFWRoleID);
+      replyMessage = '<:nindy_yes:977817511821213757> done, role NSFW **added**!';
+    } else {
+      await commandType.member.roles.remove(NSFWRoleID);
+      replyMessage = '<:nindy_yes:977817511821213757> done, role NSFW **removed**!';
     }
 
     if (interaction !== null) { // slash
       client.cmdlog(interaction.user.username, interaction.commandName, []);
-      const replyMessage = await setRole(interaction);
       interaction.reply({ content: replyMessage, ephemeral: true });
     } else { // prefix
-      const replyMessage = await setRole(message);
       message.reply({
         content: replyMessage, allowedMentions: { repliedUser: false }
       })
